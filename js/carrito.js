4c405f02ae0caa25c749fb3fbdd304b69310be94
Lautaro Perez <lautarop28@gmail.com>
2022-08-11 20:56:26 -0300

    primera entrega del proyecto final js

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,4 @@
-//creo un array de objetos (productos)
+//array de objetos (productos)
 const productos=[
     {
         tipo: "vino",
@@ -31,81 +31,68 @@ const productos=[
 
 ];
 
-//el cliente ingresa el codigo de lo que quiere comprar
-let codigo=parseInt(prompt("¿Que desea comprar? 1: vino, 2: cerveza, 3: espumante, 4:espirituosa. (-1 para finalizar compra)"));
+//declaracion
+let codigo;
 let precioPorCodigo;
-let sumaProductos=0;
 let cantidadProductos=0;
+let carrito = [];
 
-//busca el precio del producto en funcion del codigo ingresado
-for (const producto of productos){
-    if(producto.codigo == codigo){
-        precioPorCodigo = producto.precio;
-        console.log (precioPorCodigo);
-    }
-}
-
-//funcion para sumar los productos
-function calcularProductos () {
-    while(codigo!=-1){
-        //va sumando los precios de los productos
-        sumaProductos=sumaProductos+precioPorCodigo;
-        //suma de a uno en uno la cantidad de productos
-        cantidadProductos++;
-        //se repite el ciclo
+//funcion para ir armando el carrito
+function armarCarrito(){
+    //bucle en el que pregunta al cliente qué producto desea comprar en funcion de un codigo, y los va sumando al carrito teniendo en cuenta su precio.
+    while (codigo!=-1) {
         codigo=parseInt(prompt("¿Que desea comprar? 1: vino, 2: cerveza, 3: espumante, 4:espirituosa. (-1 para finalizar compra)"));
-    }
-    //devuelve la cantidad de productos que lleva y el precio total a pagar.
-    alert("Usted compro "+cantidadProductos+" producto/s, por un valor total de $"+sumaProductos);
-}
+       
+        for (const producto of productos){
+            if(producto.codigo == codigo){
+                precioPorCodigo = producto.precio;
+                console.log (precioPorCodigo);
+            }
+        }
+    
+        carrito.push(precioPorCodigo); //metodo push
 
-//ejecuta la funcion
-calcularProductos();
-  
-
-/* ---------------------------------------------------------------------------------------------------------------------------------------
-CODIGO ANTERIOR:
-
-//el cliente ingresa el precio del producto que quiere comprar
-let precioProducto=parseInt(prompt("Ingresa el precio del producto (-1 para salir)"));
-let sumaProductos=0;
-let cantidadProductos=0;
-
-//bucle para continuar preguntado precio de productos, por si quiere seguir comprando.
-function calculoProductos () {
-    while(precioProducto!=-1){
-        //va sumando los precios de los productos
-        sumaProductos=sumaProductos+precioProducto;
         //suma de a uno en uno la cantidad de productos
         cantidadProductos++;
-        //se repite el ciclo
-        precioProducto=parseInt(prompt("Ingresa el precio del producto (-1 para salir)"));
     }
-    //devuelve la cantidad de productos que lleva y el precio total a pagar.
-    alert("Usted compro "+cantidadProductos+" producto/s, por un valor total de $"+sumaProductos);
+
+    //verifico en consola
+    console.log(carrito);
+    console.log(cantidadProductos);
 }
 
-//ejecuta la funcion del calculo de productos
-calculoProductos();
+//ejecuto la funcion
+armarCarrito();
 
-//pregunta por el metodo de pago y ofrece descuento en efectivo.
+//funcion de orden superior (reduce): para sumar el total del carrito. 
+let sumaCarrito = carrito.reduce(((acumulador, producto) => acumulador + precioPorCodigo),0);
+
+//verifico en consola
+console.log(sumaCarrito);
+
+//salida para el cliente
+alert("Usted compro "+cantidadProductos+" producto/s, por un valor total de $"+sumaCarrito);
+
+//pregunta al cliente por el metodo de pago y ofrece descuento en efectivo.
 let metodoDePago=prompt("Ingrese 1 para pagar en efectivo con 10% de descuento, o 2 para pagar con tarjeta");
 
+//funcion para pagar el carrito
 function pagarCarrito () {
     if (metodoDePago == 1) {
     
         //funcion para el descuento
         function descuento () {
-            return sumaProductos * 0.9;
+            return sumaCarrito * 0.9;
         }
     
         let totalConDesc = descuento();
         alert("Ha seleccionado pagar en efectivo. El total a pagar con descuento es $"+totalConDesc);
     
     }else if (metodoDePago == 2) {
-        alert("Ha seleccionado pagar con tarjeta. El total a pagar es $"+sumaProductos);
+        alert("Ha seleccionado pagar con tarjeta. El total a pagar es $"+sumaCarrito);
     }
+
 }
 
 //ejecuta la funcion del pago de la compra
-pagarCarrito(); */
+pagarCarrito();
