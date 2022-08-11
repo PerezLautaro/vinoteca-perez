d3d0773a561781ee46f488a958883fa43337cddb
Lautaro Perez <lautarop28@gmail.com>
2022-08-10 21:26:26 -0300

    preentrega 1 en proceso

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,71 @@
+//creo un array de objetos (productos)
+const productos=[
+    {
+        tipo: "vino",
+        nombre: "Luiggi Bosca",
+        bodega: "Luiggi Bosca",
+        precio: 1600,
+        codigo: 1
+    },
+    {
+        tipo: "cerveza",
+        nombre: "Amber Lager",
+        bodega: "Patagonia",
+        precio: 750,
+        codigo: 2
+    },
+    {
+        tipo: "espumante",
+        nombre: "Champagne Mumm Extra Brut",
+        bodega: "Mumm",
+        precio: 950,
+        codigo: 3
+    },
+    {
+        tipo: "espirituosa",
+        nombre: "Ron Bacardi Carta Oro",
+        bodega: "Bacardi",
+        precio: 1300,
+        codigo: 4
+    }
+
+];
+
+//el cliente ingresa el codigo de lo que quiere comprar
+let codigo=parseInt(prompt("¿Que desea comprar? 1: vino, 2: cerveza, 3: espumante, 4:espirituosa. (-1 para finalizar compra)"));
+let precioPorCodigo;
+let sumaProductos=0;
+let cantidadProductos=0;
+
+//busca el precio del producto en funcion del codigo ingresado
+for (const producto of productos){
+    if(producto.codigo == codigo){
+        precioPorCodigo = producto.precio;
+        console.log (precioPorCodigo);
+    }
+}
+
+//funcion para sumar los productos
+function calcularProductos () {
+    while(codigo!=-1){
+        //va sumando los precios de los productos
+        sumaProductos=sumaProductos+precioPorCodigo;
+        //suma de a uno en uno la cantidad de productos
+        cantidadProductos++;
+        //se repite el ciclo
+        codigo=parseInt(prompt("¿Que desea comprar? 1: vino, 2: cerveza, 3: espumante, 4:espirituosa. (-1 para finalizar compra)"));
+    }
+    //devuelve la cantidad de productos que lleva y el precio total a pagar.
+    alert("Usted compro "+cantidadProductos+" producto/s, por un valor total de $"+sumaProductos);
+}
+
+//ejecuta la funcion
+calcularProductos();
+  
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------
+CODIGO ANTERIOR:
+
 //el cliente ingresa el precio del producto que quiere comprar
 let precioProducto=parseInt(prompt("Ingresa el precio del producto (-1 para salir)"));
 let sumaProductos=0;
@@ -40,4 +108,4 @@ function pagarCarrito () {
 }
 
 //ejecuta la funcion del pago de la compra
-pagarCarrito();
+pagarCarrito(); */
